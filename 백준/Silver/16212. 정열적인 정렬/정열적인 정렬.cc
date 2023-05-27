16794c3fa9a7c250dbf8e68e5821419f8fc2b6f9
배진호 <72592302+BaeJinho4028@users.noreply.github.com>
2023-05-27 12:33:21 +0900

    [Silver V] Title: 정열적인 정렬, Time: 140 ms, Memory: 3972 KB -BaekjoonHub

---
+++ 백준/Silver/16212. 정열적인 정렬/정열적인 정렬.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a[500005];
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+
+	sort(a, a + n);
+	
+	for (int i = 0; i < n; i++) {
+		cout << a[i] << ' ';
+	}
+}
