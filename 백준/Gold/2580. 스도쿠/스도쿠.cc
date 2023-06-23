98c7a129e290b8f58cad4ae0024c8082d122630b
배진호 <72592302+BaeJinho4028@users.noreply.github.com>
2023-06-24 00:47:34 +0900

    [Gold IV] Title: 스도쿠, Time: 204 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2580. 스도쿠/스도쿠.cc
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define X first
+#define Y second
+
+int board[9][9];
+vector<pair<int, int>> v;
+int n;
+bool flag;
+
+bool chk(int x, int y, int idx) {
+	for (int i = 0; i < 9; i++) {
+		if (board[i][y] == idx) return false; //가로
+		if (board[x][i] == idx) return false; //세로
+	}
+
+	int nx = (x / 3) * 3;
+	int ny = (y / 3) * 3;
+	for (int i = nx; i < nx + 3; i++) {
+		for (int j = ny; j < ny + 3; j++) {
+			if (board[i][j] == idx) return false; // 9등븐
+		}
+	}
+
+	return true;
+}
+
+void solve(int cur) {
+	if (flag) return;
+	if (cur == n) {
+		cout << '\n';
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				cout << board[i][j] << ' ';
+			}
+			cout << '\n';
+		}
+
+		flag = true;
+		return;
+	}
+
+	int nx = v[cur].X;
+	int ny = v[cur].Y;
+	for (int i = 1; i <= 9; i++) {
+		if (chk(nx, ny, i)) {
+			board[nx][ny] = i;
+			solve(cur + 1);
+			board[nx][ny] = 0;
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			cin >> board[i][j];
+			if (board[i][j] == 0)
+				v.push_back({ i,j });
+		}
+	}
+
+	n = v.size();
+	solve(0);
+}
