cf3a4f3dc900ef998fa215941ead0d2fa8889959
배진호 <72592302+BaeJinho4028@users.noreply.github.com>
2023-06-25 02:30:01 +0900

    [Gold IV] Title: LCS 2, Time: 4 ms, Memory: 5968 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.cc
+++ 백준/Gold/9252. LCS 2/LCS 2.cc
@@ -20,23 +20,22 @@ int main() {
 
 	cout << d[a.size()][b.size()] << '\n';
 
-	//역추적 ( 공부필요) 참고 : https://hyeo-noo.tistory.com/18 
-	string ans = "";
+    //yun님코드참고
+    
+	int len = d[a.size()][b.size()];
+	int i = a.size(), j = b.size();
+	char ans[1005]{};
 
-	int idx = b.size();
-	for (int row = a.size(); row >= 1; row--) {
-		for (int col = idx; col >= 1; col--) {
-			if (d[row][col] == d[row - 1][col]) {
-				// 바로 위 dp값이 같으면 현재 열 값을 유지한다
-				idx = col;
-				break;
-			}
-			else if (d[row][col] == d[row][col - 1]) continue;
-			else 
-				ans += a[row - 1];
+	for (int k = 0; k < len; ++k) {
+		while (1) {
+			if (d[i][j] == d[i][j - 1]) --j;
+			else if (d[i][j] == d[i - 1][j]) --i;
+			else break;
 		}
+
+		ans[len - k - 1] = a[i-1];
+		--i, --j;
 	}
 
-	reverse(ans.begin(), ans.end()); //이것이 아니라 ans = a[row-1] + ans; 하는법도잇음
 	cout << ans;
 }
