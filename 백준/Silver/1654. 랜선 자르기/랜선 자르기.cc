cca7bfdf09c086cf43251e3a1e921137959e3a4a
배진호 <72592302+BaeJinho4028@users.noreply.github.com>
2023-06-27 19:30:34 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int n, k;
+int a[10005];
+
+bool solve(ll x) {
+	ll cur = 0;
+	for (int i = 0; i < k; i++) cur += a[i] / x;
+	return cur >= n;
+}
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> k >> n;
+
+	for (int i = 0; i < k; i++) cin >> a[i];
+
+	ll st = 1;
+	ll en = 0x7fffffff;
+
+	while (st < en) {
+		ll mid = (st + en + 1) / 2;
+		if (solve(mid)) st = mid;
+		else en = mid - 1;
+	}
+	cout << st;
+}
