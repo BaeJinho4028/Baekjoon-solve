2deac4ea96c10e1c5d2e0f0620da1fca18338b44
배진호 <72592302+BaeJinho4028@users.noreply.github.com>
2023-07-22 21:16:11 +0900

    [Bronze IV] Title: 과자, Time: 128 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        int d= a*b - c;
+        if(d<0) System.out.println(0);
+        else System.out.println(d);
+    }
+}
