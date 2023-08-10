779abe85463bfe3994185ccd73841707800e6185
배진호 <72592302+BaeJinho4028@users.noreply.github.com>
2023-08-11 00:21:48 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 112 ms, Memory: 13840 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.java
@@ -0,0 +1,10 @@
+import java.math.BigInteger;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        System.out.println("고려대학교");
+    }
+}
