fdd85248190c39e1703bd62aa43e6dbb2c18f9bb
배진호 <72592302+BaeJinho4028@users.noreply.github.com>
2023-08-11 21:46:43 +0900

    [Gold V] Title: 벼락치기, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Gold/14728. 벼락치기/벼락치기.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, t;
+int k, s;
+int d[10005];
+
+/*
+중복이 안되므로 뒤부터 돈다.
+중복이 된다면 앞부터 돈다.
+*/
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> t;
+
+	for (int i = 1; i <= n; i++) {
+		cin >> k >> s;
+		for (int j = t; j >= k; j--) {
+			d[j] = max(d[j], d[j - k] + s);
+		}
+	}
+
+	cout << d[t];
+}
