a8108f894f941d8aa86f1d1d19b7deb1b8043fe6
배진호 <72592302+BaeJinho4028@users.noreply.github.com>
2023-08-12 00:25:06 +0900

    [Gold V] Title: 평범한 배낭, Time: 16 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,26 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int d[105][100005]; 
-int w[105];
-int v[105];
+int n, t;
+int k, s;
+int d[100005];
 
-int main(void) {
+/*
+중복이 안되므로 뒤부터 돈다.
+중복이 된다면 앞부터 돈다.
+*/
+
+int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	int n, k;
-	cin >> n >> k;
-
-	for (int i = 1; i <= n; i++)
-		cin >> w[i] >> v[i];
+	cin >> n >> t;
 
 	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= k; j++) {
-			if (j - w[i] >= 0) 
-				d[i][j] = max(d[i - 1][j], d[i - 1][j - w[i]] + v[i]);
-			else d[i][j] = d[i - 1][j];
+		cin >> k >> s;
+		for (int j = t; j >= k; j--) {
+			d[j] = max(d[j], d[j - k] + s);
 		}
 	}
-	cout << d[n][k];
+
+	cout << d[t];
 }
