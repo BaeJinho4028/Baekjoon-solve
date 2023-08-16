0b2d0a454a4c8f6e805bf7316bdf4b645e4b9f43
배진호 <72592302+BaeJinho4028@users.noreply.github.com>
2023-08-16 21:37:14 +0900

    [Bronze IV] Title: 녹색거탑, Time: 124 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(1<<n);
+    }
+}
