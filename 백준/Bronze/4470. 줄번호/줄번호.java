899c5594eade719b00ac8eb9aa88e3319983eb1a
배진호 <72592302+BaeJinho4028@users.noreply.github.com>
2023-08-16 20:54:50 +0900

    [Bronze IV] Title: 줄번호, Time: 232 ms, Memory: 18444 KB -BaekjoonHub

---
+++ 백준/Bronze/4470. 줄번호/줄번호.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+
+        for(int i=1; i<=n;i++){
+            System.out.println(i + ". " + sc.nextLine());
+        }
+    }
+}
