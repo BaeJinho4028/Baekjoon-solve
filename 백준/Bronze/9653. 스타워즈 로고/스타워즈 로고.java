48b6cefa4be7b116c973257b3437bbbf1aa1f6a6
배진호 <72592302+BaeJinho4028@users.noreply.github.com>
2023-08-23 20:48:57 +0900

    [Bronze V] Title: 스타워즈 로고, Time: 120 ms, Memory: 14008 KB -BaekjoonHub

---
+++ 백준/Bronze/9653. 스타워즈 로고/스타워즈 로고.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("    8888888888  888    88888");
+        System.out.println("   88     88   88 88   88  88");
+        System.out.println("    8888  88  88   88  88888");
+        System.out.println("       88 88 888888888 88   88");
+        System.out.println("88888888  88 88     88 88    888888");
+        System.out.println("");
+        System.out.println("88  88  88   888    88888    888888");
+        System.out.println("88  88  88  88 88   88  88  88");
+        System.out.println("88 8888 88 88   88  88888    8888");
+        System.out.println(" 888  888 888888888 88  88      88");
+        System.out.println("  88  88  88     88 88   88888888");
+    }
+}
