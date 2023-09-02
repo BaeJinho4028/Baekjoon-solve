3377f6d2c125a0d6104267506d48d923c11e2413
배진호 <72592302+BaeJinho4028@users.noreply.github.com>
2023-09-02 23:11:41 +0900

    [Gold V] Title: 잠수함식별, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/2671. 잠수함식별/잠수함식별.cc
+++ 백준/Gold/2671. 잠수함식별/잠수함식별.cc
@@ -9,13 +9,36 @@ using namespace std;
 int dx[8] = { 1,0,-1,0, 1, 1, -1, -1 };
 int dy[8] = { 0,1,0,-1, 1, -1, 1, -1 };
 
+int n;
+string s;
+
+bool chk() {
+	int i = 0;
+	while(s[i]) {
+		if (s[i++] == '1') { //1시작
+			int cnt = 0;
+			while (s[i] == '0') //다음 0이 1개이상 나와야함
+				i++, cnt++;
+			if (cnt < 2) return false;
+
+			cnt = 0; //개수 2개이상인경우 / 끝나거나, 다음1이거나, 다다음1이거나
+			while (s[i] == '1' && (s[i + 1] == 0 || s[i + 1] == '1' || s[i + 2] == '1'))
+				i++, cnt++;
+			if (cnt < 1) return false;
+		}
+		else { //0시작 : 다음이 1인지 확인
+			if (s[i] != '1') return false;
+			i++;
+		}
+	}
+	return true;
+}
+
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	string s;
 	cin >> s;
 
-	regex pattern("(100+1+|01)+");
-	cout << (regex_match(s, pattern) ? "SUBMARINE" : "NOISE");
+	cout << (chk() ? "SUBMARINE" : "NOISE");
 }
