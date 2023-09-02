e05d3db54f28f73372439a43f29947df71362d30
배진호 <72592302+BaeJinho4028@users.noreply.github.com>
2023-09-02 21:57:42 +0900

    [Gold V] Title: Contact, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1013. Contact/Contact.cc
+++ 백준/Gold/1013. Contact/Contact.cc
@@ -8,54 +8,30 @@ using namespace std;
 #define ll long long
 int dx[8] = { 1,0,-1,0, 1, 1, -1, -1 };
 int dy[8] = { 0,1,0,-1, 1, -1, 1, -1 };
-int n, m;
-int mx, mn = '????';
-int ans, cnt;
 
+int n;
+string s;
 
-//1000 11 01 111
-void solve() {
-	string s;
-	cin >> s;
+bool chk() {
+	int i = 0;
+	while(s[i]) {
+		if (s[i++] == '1') { //1시작
+			int cnt = 0;
+			while (s[i] == '0') //다음 0이 1개이상 나올 경우 
+				i++, cnt++;
+			if (cnt < 2) return false;
 
-	int idx = 0;
-	bool flag = true;
-
-	while (idx < s.size() && flag) {
-		switch (s[idx]) {
-		case '0': //다음이 1인지 체크
-			idx++;
-			if (s.size() <= idx || s[idx] == '0')
-				flag = false;
-			idx++;
-			break;
-
-		case '1':
-			idx++;
-			int nxt1 = s.find('1', idx); //다음 1이 무조건 두칸보다 뒤에 나와야함
-			if (nxt1 == string::npos || nxt1 - idx < 2) {
-				flag = false;
-				break;
-			}
-
-			int nxt0 = s.find('0', nxt1);
-			if (nxt0 == s.size() - 1) { 
-				flag = false;
-				break;
-			}
-			if (nxt0 == string::npos) {
-				idx = s.size();
-				break;
-			}
-			idx = nxt0;
-
-			if ('0' == s[nxt0 + 1] && nxt0 - nxt1 > 1)
-				idx--;
-			break;
+			cnt = 0;
+			while (s[i] == '1' && (s[i + 1] == 0 || s[i + 1] == '1' || s[i + 2] == '1'))
+				i++, cnt++;
+			if (cnt < 1) return false;
+		}
+		else { //0시작 : 다음이 1인지 확인
+			if (s[i] != '1') return false;
+			i++;
 		}
 	}
-
-	cout << (idx == s.size() && flag ? "YES" : "NO") << '\n';
+	return true;
 }
 
 int main() {
@@ -66,6 +42,7 @@ int main() {
 	cin >> t;
 
 	while (t--) {
-		solve();
+		cin >> s;
+		cout << (chk()? "YES\n" : "NO\n");
 	}
 }
