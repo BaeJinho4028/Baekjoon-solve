93f7fe74488e08d26714b88c286af4c4793e1872
배진호 <72592302+BaeJinho4028@users.noreply.github.com>
2023-11-16 21:30:33 +0900

    [Gold II] Title: 사전, Time: 0 ms, Memory: 2188 KB -BaekjoonHub

--- 백준/Gold/1256. 사전/사전.cc
+++ 백준/Gold/1256. 사전/사전.cc
@@ -4,50 +4,6 @@ typedef long long ll;
 #define X first
 #define Y second
 
-/*
-  1 1
-
-  1 : az
-  2 : za
-  
-
-  2 1
-
-  1 : aaz
-  2 : aza
-  3 : zaa
-
-
-  1 2
-
-  1 : azz
-  2 : zaz
-  3 : zza
-
-
-  2 2   4C2
-
-  1 : aazz
-  2 : azaz
-  3 : azza
-  4 : zaaz
-  5 : zaza
-  6 : zzaa
-
-  1 3 
-
-  1 : azzz
-  2 : zazz
-  3 : zzaz
-  4 : zzza
-
-  2 3
-
-  1 : aazzz
-  2 : azazz
-  3 : 
-*/
-
 ll d[205][105];
 
 ll comb(int n, int r) {
@@ -58,9 +14,10 @@ ll comb(int n, int r) {
 	ll a = comb(n - 1, r - 1);
 	ll b = comb(n - 1, r);
 	if (a + b > 1000000000) return ret = 0x3f3f3f3f3f3f3f3f;
+
 	return d[n][r] = comb(n-1, r-1) + comb(n - 1, r);
 }
-//
+
 void f(int n, int m, ll k) {
 	ll tmp = comb(n + m - 1, n - 1);
 	if (n > 0 && k <= tmp) {
