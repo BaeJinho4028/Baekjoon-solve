6ec72c42a1a8e3306db7538df2704b4e2b65f27f
배진호 <72592302+BaeJinho4028@users.noreply.github.com>
2023-12-12 10:20:04 +0900

    [Bronze I] Title: APC는 왜 서브태스크 대회가 되었을까?, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/17224. APC는 왜 서브태스크 대회가 되었을까？/APC는 왜 서브태스크 대회가 되었을까？.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define X first
+#define Y second
+#define ll long long
+
+vector<pair<int, int>> v;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n, l, k;
+	cin >> n >> l >> k;
+
+	for (int i = 0; i < n; i++) {
+		int a, b;
+		cin >> a >> b;
+
+		v.push_back({ b,a });
+	}
+	sort(v.begin(), v.end());
+
+	int ans = 0;
+	for (int i = 0; i < n; i++) {
+		if (l >= v[i].X) {
+			ans += 140;
+			k--;
+		}
+		else if (l >= v[i].Y) {
+			ans += 100;
+			k--;
+		}
+		if (k == 0) break;
+	}
+	cout << ans;
+}
