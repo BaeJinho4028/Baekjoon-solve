2163d7d638d6eb99e9a62ba3fe534bf20b0a3567
배진호 <72592302+BaeJinho4028@users.noreply.github.com>
2023-12-13 10:51:30 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define X first
+#define Y second
+#define ll long long
+
+vector<pair<int, int>> v;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	string s;
+	cin >> s;
+
+	cout << ":fan::fan::fan:\n";
+	cout << ":fan::" << s << "::fan:\n";
+	cout << ":fan::fan::fan:\n";
+}
