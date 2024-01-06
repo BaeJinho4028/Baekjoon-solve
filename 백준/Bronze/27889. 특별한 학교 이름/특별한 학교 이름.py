0f0636bfc2e5b5877b9a524ee8b7a5019ada7327
배진호 <72592302+BaeJinho4028@users.noreply.github.com>
2024-01-06 22:15:56 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,5 @@
+x = ['North London Collegiate School', 'Branksome Hall Asia', 'Korea International School', 'St. Johnsbury Academy']
+y = ['NLCS', 'BHA', 'KIS', 'SJA']
+
+a = input()
+print(x[y.index(a)])
