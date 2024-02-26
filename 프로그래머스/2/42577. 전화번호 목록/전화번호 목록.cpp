76091b8380e97dfbbd755154ae44b13d12f99356
배진호 <72592302+BaeJinho4028@users.noreply.github.com>
2024-02-26 17:11:16 +0900

    [level 2] Title: 전화번호 목록, Time: 114.36 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool solution(vector<string> phone_book) {
+    bool answer = true;
+    
+    sort(phone_book.begin(), phone_book.end());
+    
+    for(int i=1; i<phone_book.size(); i++){
+        if(phone_book[i-1] == phone_book[i].substr(0, phone_book[i-1].size())) return false;
+    }
+    
+    return answer;
+}
