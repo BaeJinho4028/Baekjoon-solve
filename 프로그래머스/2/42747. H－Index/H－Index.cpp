cc82070417eabb5b1b2d9bab1d79d22f1de92308
배진호 <72592302+BaeJinho4028@users.noreply.github.com>
2024-02-27 17:09:11 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+    int answer = 0;
+    
+    sort(citations.begin(), citations.end(), greater<>());
+    
+    for(int i=0; i<citations.size(); i++){
+        if(citations[i] >= i + 1){
+            answer = i + 1;
+        }    
+    }
+    
+    return answer;
+}
